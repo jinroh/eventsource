fdcee3ba9046af6b699fbdff8225aaf15f0b978d
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2014-01-27 13:06:44 +0100

    Update examples

--- examples/greeter.go
+++ examples/greeter.go
@@ -1,27 +1,24 @@
 package main
 
 import (
-	eventsource "../../http"
+	eventsource "../"
 	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
-	es := eventsource.New(nil, nil)
+	es := eventsource.New()
 	defer es.Close()
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 	http.Handle("/events", es)
 	go func() {
 		for {
-			es.SendMessage("hello", "", "")
+			es.SendMessage([]byte("hello"), "message")
 			log.Printf("Hello has been sent (consumers: %d)", es.ConsumersCount())
 			time.Sleep(2 * time.Second)
 		}
 	}()
 	log.Print("Open URL http://localhost:8080/ in your browser.")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
